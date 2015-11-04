bd39ad79b982ecc8a482c2868f435d9607d9880d
NivedhaSenthil <nivedhasenthil@gmail.com>
2015-11-04 11:26:05 +0530

    Formating

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var server = require("./server");
 var router = require("./routes");
 var requestHandlers = require("./requestHandlers");
 var handle = {}
+
 handle["/"] = requestHandlers.start; 
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
+
 server.start(router.route, handle);
